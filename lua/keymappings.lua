d04988e573d4dd5cf85f42deeceb78cd4a37318f
Omkar Nandan <omknan60@gmail.com>
2021-06-24 14:30:19 +0530

    changes to keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -32,3 +32,4 @@ vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin'
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>b', "<cmd>lua require('telescope.builtin').file_browser()<cr>", {noremap = true, silent = true})
