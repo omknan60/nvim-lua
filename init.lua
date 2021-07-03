85b5e30eed8a19f5206b7ff5e9a2d613c1ddea71
omknan60 <omknan60@gmail.com>
2021-07-03 22:16:16 +0530

    removed telescope

--- init.lua
+++ init.lua
@@ -8,13 +8,6 @@ vim.api.nvim_exec(
 [[
 call plug#begin('~/AppData/Local/nvim-data/site/pack/plugged/start')
   Plug 'neovim/nvim-lspconfig'
-  
-  "Plugins required for telescope
-  Plug 'nvim-lua/popup.nvim'
-  Plug 'nvim-lua/plenary.nvim'
-  Plug 'nvim-telescope/telescope.nvim'
-  Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
-        
   Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 call plug#end()
 ]]
