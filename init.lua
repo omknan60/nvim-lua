d918ecababdce103dad3157707ef303047bd6e7e
omknan60 <omknan60@gmail.com>
2021-07-14 16:24:51 +0530

    removed lsp for the time being will add later

--- init.lua
+++ init.lua
@@ -2,13 +2,11 @@ require('colorscheme')
 require('sets')
 require('keymappings')
 require('variables')
---require('lspinstallconfig')
 
 vim.api.nvim_exec(
 [[
 call plug#begin('~/AppData/Local/nvim-data/site/pack/plugged/start')
   Plug 'tpope/vim-vinegar'
-  Plug 'neovim/nvim-lspconfig'
   Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 call plug#end()
 ]]
